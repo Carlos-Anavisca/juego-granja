f0bce61cb94e4cfa9ec991dfe786e1437fc66f5a
¨Edwin <anaviscacarlos123@gmail.com>
2018-10-16 14:34:13 -0600

    Dibujando el fondo de pantalla

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById('fondo');
+var lapiz = canvas.getContext('2d');
+
+var fondo = {
+    url: './imagenes/tile.png',
+    imagen: Image
+};
+
+fondo.imagen= new Image()
+fondo.imagen.src = fondo.url;
+
+fondo.imagen.addEventListener("load", function(){
+    dibujar();
+});
+
+function dibuar(){
+    lapiz.drawImage(fondo.imagen,0,0);
+}
